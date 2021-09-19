2276ed6e4790df9e39f9adc9fb5153d453c76a3f
Petr Bezlepkin <bezl_petr@mail.ru>
2021-09-19 10:35:46 +0300

    Исправляет замечания по результатам проверки

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,7 +1,13 @@
 var navMain = document.querySelector(".main-nav");
 var navToggle = document.querySelector(".main-nav__toggle");
+var map = document.querySelector(".page-footer__map");
+var mapImage = document.querySelector(".page-footer__map-image");
+var mapGoogle = document.querySelector(".page-footer__map-google");
 
 navMain.classList.remove("main-nav--nojs");
+map.classList.remove("page-footer__map--nojs");
+mapImage.classList.remove("page-footer__map-image--nojs");
+mapGoogle.classList.remove("page-footer__map-google--nojs");
 
 navToggle.addEventListener("click", function () {
   if (navMain.classList.contains("main-nav--closed")) {
